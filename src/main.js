8a2394c3bfec46d1028a30e6c944fc08ad8d5206
Henrique Faria <hrfaria@gmail.com>
2020-07-25 21:01:16 -0600

    Re-added bootstrap-vue with 'vue add'

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App'
 import router from './router'
 import store from './store';
@@ -24,6 +27,7 @@ new Vue({
   el: '#app',
   router,
   store,
-  components: { App },
+  // components: { App },
+  render: h => h(App),
   template: '<App/>'
 })
