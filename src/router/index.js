316ed93cff3cecc1565de4738b15808c7cdc91c9
Henrique Faria <hrfaria@gmail.com>
2020-07-28 21:24:47 -0600

    Router component lazy load

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
-import MXAsset from '@/components/MXAsset'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -16,7 +16,7 @@ export default new Router({
       path: '/asset/:assetuid',
       name: 'MXAsset',
       props: true,
-      component: MXAsset
+      component: () => import('@/components/MXAsset')
     },
     {
       path: '/about',
